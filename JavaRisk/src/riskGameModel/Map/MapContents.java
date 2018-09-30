d53538a0b2663ebb14e3e652de019bd20e9df6ff
12sandeep1994 <sandeepsiddaramaiah@gmail.com>
2018-09-30 12:04:54 -0400

    Changes made to create the map file. (Its displayimg on the console now, yet to do the final touches and have to add javadoc)

--- JavaRisk/src/riskGameModel/Map/MapContents.java
+++ JavaRisk/src/riskGameModel/Map/MapContents.java
@@ -10,4 +10,32 @@ public class MapContents {
 
 	private HashMap<Country, List<Country>> countryAndNeighbors = new HashMap<>();
 	private HashMap<Continent, List<Country>> continentAndItsCountries = new  HashMap<>();
+	/**
+	 * This method is to get Hashmap of countries and its list of neighbors.
+	 * @return  countryAndNeighbors Hashmap of countries and its list of neighbors.
+	 */
+	public HashMap<Country, List<Country>> getCountryAndNeighbors() {
+		return countryAndNeighbors;
+	}
+	/**
+	 * This method is to set Hashmap of countries and its list of neighbors.
+	 * @param countryAndNeighbors Hashmap of countries and its list of neighbors.
+	 */
+	public void setCountryAndNeighbors(HashMap<Country, List<Country>> countryAndNeighbors) {
+		this.countryAndNeighbors = countryAndNeighbors;
+	}
+	/**
+	 * Method to get Continent and its countries
+	 * @return  continentAndItsCountries Hashmap of continents and its list of countries.
+	 */
+	public HashMap<Continent, List<Country>> getContinentAndItsCountries() {
+		return continentAndItsCountries;
+	}
+	/**
+	 * Method to set Continent and its countries
+	 * @param continentAndItsCountries Hashmap of continents and its list of countries.
+	 */
+	public void setContinentAndItsCountries(HashMap<Continent, List<Country>> continentAndItsCountries) {
+		this.continentAndItsCountries = continentAndItsCountries;
+	}
 }
