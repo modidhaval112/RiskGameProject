bb4a215244bfd35714cf533d4330d73cb1b760c8
Darwin Anirudh <darwinanirudh@gmail.com>
2018-10-04 13:31:28 -0400

    Changed the name game launcher as this is the main file. Old game launcher which contains create map is renamed to GameLauncher_createMap.

---
+++ JavaRisk/src/riskGameView/GameLauncher_createMap.java
@@ -0,0 +1,29 @@
+package riskGameView;
+
+import java.io.File;
+
+public class GameLauncher {
+
+	public static void main(String[] args) {
+
+		GameLauncher gameLauncher = new GameLauncher();
+		gameLauncher.createMap();
+		
+	}
+
+	private void createMap() {
+		CreateMapFile createMapFile = new CreateMapFile();
+		//createMapFile.main(null);
+	}
+
+	public void editMap() {
+			MapRender mapRender = new MapRender();
+			File selectedFile = mapRender.chooseFile();
+			if(selectedFile!=null) {
+				mapRender.readFile();
+			}
+	}
+
+	
+
+}
