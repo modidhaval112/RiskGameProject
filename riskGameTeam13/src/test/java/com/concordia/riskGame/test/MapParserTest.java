20a6c5859e9390cf09a3c7b722b72c61b83a85ba
Dheeraj As <43119091+RealNameHidden@users.noreply.github.com>
2018-10-06 12:22:33 -0400

    Added getContinentList function
    Changed variable name from contitentList to continentList

---
+++ riskGameTeam13/src/test/java/com/concordia/riskGame/test/MapParserTest.java
@@ -0,0 +1,30 @@
+package com.concordia.riskGame.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.concordian.riskGame.Controller.MapParseController;
+
+public class MapParserTest {
+
+	private String filePath;
+	private MapParseController mapParserObject;
+	@Before
+	public void setUp() throws Exception {
+		filePath="src/main/resources/Africa.map";
+		mapParserObject = new MapParseController();		
+	}
+	
+
+	@Test
+	public void test() {
+		mapParserObject.MapParser(filePath);
+		int numberOfContinents= mapParserObject.getContinentList().size();
+		
+		assertEquals(7,numberOfContinents);
+	}
+		
+
+}
