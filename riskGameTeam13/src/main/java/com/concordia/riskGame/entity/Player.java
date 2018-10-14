7616e368d634080a52d983380cc024851a35f108
Dheeraj As <43119091+RealNameHidden@users.noreply.github.com>
2018-10-14 14:42:11 -0400

    Added Game Drive and player files
    @darwinanirudh

---
+++ riskGameTeam13/src/main/java/com/concordia/riskGame/entity/Player.java
@@ -0,0 +1,65 @@
+package com.concordia.riskGame.entity;
+
+
+
+
+import javax.swing.*;
+import javax.swing.plaf.basic.BasicIconFactory;
+import java.io.File;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.*;
+
+/**
+ * This is entity class to set and get properties of player.
+ *
+ * @author Darwin Anirudh G
+ */
+public class Player  implements Serializable {
+	
+    public String name;
+    public int currentPlayerReinforceArmies;
+    public int playerCount;
+    public int totalArmies;
+    public int reinforcementArmies;
+    public List<Country> assignedCountries;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCurrentPlayerReinforceArmies() {
+		return currentPlayerReinforceArmies;
+	}
+	public void setCurrentPlayerReinforceArmies(int currentPlayerReinforceArmies) {
+		this.currentPlayerReinforceArmies = currentPlayerReinforceArmies;
+	}
+	public int getPlayerCount() {
+		return playerCount;
+	}
+	public void setPlayerCount(int playerCount) {
+		this.playerCount = playerCount;
+	}
+	public int getTotalArmies() {
+		return totalArmies;
+	}
+	public void setTotalArmies(int totalArmies) {
+		this.totalArmies = totalArmies;
+	}
+	public int getReinforcementArmies() {
+		return reinforcementArmies;
+	}
+	public void setReinforcementArmies(int reinforcementArmies) {
+		this.reinforcementArmies = reinforcementArmies;
+	}
+	public List<Country> getAssignedCountries() {
+		return assignedCountries;
+	}
+	public void setAssignedCountries(List<Country> assignedCountries) {
+		this.assignedCountries = assignedCountries;
+	}
+
+    
+ }
+
