2dbe9b7b81c07777b468848d6df0533de190996b
Dheeraj As <43119091+RealNameHidden@users.noreply.github.com>
2018-10-14 14:42:11 -0400

    Added Game Drive and player files
    @darwinanirudh

---
+++ riskGameTeam13/src/main/java/com/concordian/riskGame/View/GameDriver.java
@@ -0,0 +1,66 @@
+package com.concordian.riskGame.Controller;
+
+import java.util.Scanner;
+
+import com.concordia.riskGame.util.ReadConfiguration;
+import com.concordian.riskGame.View.PlayerCount;
+
+public class GameDriver {
+
+	private PlayerCount playerCountOBject;
+	private ReadConfiguration readConfigObject;
+	
+	
+	public void gamePhase()
+	{
+		System.out.println("####### Concordia Conquest Begins ####### :");
+		
+		readConfigObject = new ReadConfiguration();
+		
+		System.out.println("####### The number of players is ########## :"+readConfigObject.getPlayerCount());
+		
+		for (int i=0;i < Integer.parseInt(readConfigObject.getPlayerCount()) ;i++ )
+		{
+			
+			reinforcePhase(i);
+			attackPhase(i);
+			forfeitPhase(i);
+			
+		}
+		
+	}
+	
+	private  void forfeitPhase(int i) {
+		
+		System.out.println("Player "+i  +" forfeits");
+		
+	}
+
+	private  void attackPhase(int i) {
+		
+		
+		
+		System.out.println("Do you wish to attack : yes/no");
+		String choice = null;
+		Scanner sc =  new Scanner(System.in);
+		choice = sc.nextLine();
+		
+		if (choice.equalsIgnoreCase("yes"))
+		{
+			System.out.println("Player "+i  +" attacks");
+			attackPhase(i);
+		}
+		else
+		{
+			System.out.println("Invalid Option");
+		}
+	}
+
+	private  void reinforcePhase(int i) {
+		
+		System.out.println("Player "+i  +" reinforces");
+		
+	}
+
+	
+}
