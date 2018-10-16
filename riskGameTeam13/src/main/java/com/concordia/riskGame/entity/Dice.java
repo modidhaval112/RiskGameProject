2977f6d0a13d262296de5dd9ebd28248f5ae353f
modidhaval112 <modidhaval112@gmail.com>
2018-10-16 17:34:24 -0400

    [D_Modi][RiskGameProject]
    Formatted the class and added javaDoc

--- riskGameTeam13/src/main/java/com/concordia/riskGame/entity/Dice.java
+++ riskGameTeam13/src/main/java/com/concordia/riskGame/entity/Dice.java
@@ -8,54 +8,51 @@ import java.util.Random;
  * @author Dheeraj As
  */
 public class Dice {
-	
+
 	private int numberOfDice;
 	private int[] resultOfDice = new int[3];
 	private static int diceBound = 7;
-	
-		
+
 	/**
 	 * This Method implements the random dice rolling
+	 * 
 	 * @param numberOfDice
 	 * @return diceBox Dice Object
 	 */
 	public static Dice rollDice(int numberOfDice) {
-		
-		Dice diceBox= new Dice();
-				
-		for(int i=0; i<numberOfDice; i++) {
+
+		Dice diceBox = new Dice();
+		for (int i = 0; i < numberOfDice; i++) {
 			Random rollRandom = new Random();
-			diceBox.resultOfDice[i]=rollRandom.nextInt(diceBound-1)+1; // (diceBound-1)+1 is to exclude zero and also to make 6 inclusive
+			diceBox.resultOfDice[i] = rollRandom.nextInt(diceBound - 1) + 1; // (diceBound-1)+1 is to exclude zero and
+																				// also to make 6 inclusive
 		}
-		
 		return diceBox;
-		
+
 	}
-	
-	
+
 	/**
 	 * This method sets number of Dice
 	 * @param numberOfDice
 	 */
 	public void setNumberOfDice(int numberOfDice) {
-	    this.numberOfDice = numberOfDice;
+		this.numberOfDice = numberOfDice;
 	}
-	
-    /**
-     * this method returns number of dice. 
-     * @return noberOfDice
-     */
+
+	/**
+	 * This method returns number of dice.
+	 * @return noberOfDice
+	 */
 	public int getNumberOfDice() {
 		return numberOfDice;
 	}
-	
+
 	/**
 	 * This method returns integer array pf resulted dice.
 	 * @return resultOfDice Result Dice Array
 	 */
-	public int[] getDiceRollResult () {
+	public int[] getDiceRollResult() {
 		return resultOfDice;
-	}	
-		
-}
+	}
 
+}
--- riskGameTeam13/src/main/java/com/concordia/riskGame/entity/Player.java
+++ riskGameTeam13/src/main/java/com/concordia/riskGame/entity/Player.java
@@ -1,79 +1,147 @@
 package com.concordia.riskGame.entity;
 
-
-
-
-import javax.swing.*;
-import javax.swing.plaf.basic.BasicIconFactory;
-import java.io.File;
-import java.io.IOException;
 import java.io.Serializable;
-import java.util.*;
+import java.util.List;
+import java.util.Map;
 
 /**
  * This is entity class to set and get properties of player.
  *
  * @author Darwin Anirudh G
  */
-public class Player  implements Serializable {
-	
-    public String name;
-    public int currentPlayerReinforceArmies;
-    public int playerCount;
-    public int totalArmies;
-    public int reinforcementArmies;
-    public List<Country> assignedCountries;
-    public Map<Player, List<Country>> playerAssign;
-    public Player() {
-    	
+public class Player implements Serializable {
+
+	private String name;
+	private int currentPlayerReinforceArmies;
+	private int playerCount;
+	private int totalArmies;
+	private int reinforcementArmies;
+	private List<Country> assignedCountries;
+	private Map<Player, List<Country>> playerAssign;
+
+	/**
+	 * default constructor
+	 */
+	private Player() {
 	}
+
+	/**
+	 * Parameterized constructor to set player Name
+	 * @param name
+	 */
 	public Player(String name) {
 		this.name = name;
 	}
+
+	/**
+	 * This method is used to return Map of Player and Countries assigned to him.
+	 * @returnplayerAssign Players and assigned countries to him
+	 */
 	public Map<Player, List<Country>> getPlayerAssign() {
 		return playerAssign;
 	}
+
+	/**
+	 * This method sets map of player and countries assigned to him.
+	 * @param playerAssign
+	 */
 	public void setPlayerAssign(Map<Player, List<Country>> playerAssign) {
 		this.playerAssign = playerAssign;
 	}
 
+	/**
+	 * This method returns player name
+	 * @return name Player Name
+	 */
 	public String getName() {
 		return name;
 	}
+
+	/**
+	 * This method sets player name
+	 * @param name Player Name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	/**
+	 * This method returns number of Armies player have
+	 * @return currentPlayerReinforceArmies number of Armies player have
+	 */
 	public int getCurrentPlayerReinforceArmies() {
 		return currentPlayerReinforceArmies;
 	}
+
+	/**
+	 * This method sets number of Armies player have
+	 * @param currentPlayerReinforceArmies  number of armies player have
+	 */
 	public void setCurrentPlayerReinforceArmies(int currentPlayerReinforceArmies) {
 		this.currentPlayerReinforceArmies = currentPlayerReinforceArmies;
 	}
+
+	/**
+	 * This method returns number of players
+	 * @return playerCount no of players
+	 */
 	public int getPlayerCount() {
 		return playerCount;
 	}
+
+	/**
+	 * This method sets number of players
+	 * @param playerCount number of players
+	 */
 	public void setPlayerCount(int playerCount) {
 		this.playerCount = playerCount;
 	}
+
+	/**
+	 * This method is returns total number of armies
+	 * @return totalArmies number of Armies
+	 */
 	public int getTotalArmies() {
 		return totalArmies;
 	}
+
+	/**
+	 * This method sets total number of armies
+	 * @param totalArmies number of armies
+	 */
 	public void setTotalArmies(int totalArmies) {
 		this.totalArmies = totalArmies;
 	}
+
+	/**
+	 * This method returns number of reinforcement armies.
+	 * @return reinforcementArmies number of reinforcement armies
+	 */
 	public int getReinforcementArmies() {
 		return reinforcementArmies;
 	}
+
+	/**
+	 * This method sets number of reinforcement armies
+	 * @param reinforcementArmies number of reinforcement armies
+	 */
 	public void setReinforcementArmies(int reinforcementArmies) {
 		this.reinforcementArmies = reinforcementArmies;
 	}
+
+	/**
+	 * This method returns list of countries assigned to player
+	 * @return assignedCountries list of countries
+	 */
 	public List<Country> getAssignedCountries() {
 		return assignedCountries;
 	}
+
+	/**
+	 * This method sets list of countries assigned to player
+	 * @param assignedCountries list of countries
+	 */
 	public void setAssignedCountries(List<Country> assignedCountries) {
 		this.assignedCountries = assignedCountries;
 	}
-
-    
- }
-
+}
