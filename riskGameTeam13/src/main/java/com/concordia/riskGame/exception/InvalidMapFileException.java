19954518c67f82b427ad888246d6f7050c48de25
Darwin Anirudh <darwinanirudh@gmail.com>
2018-10-19 21:54:29 -0400

    Aligned the code.

--- riskGameTeam13/src/main/java/com/concordia/riskGame/exception/InvalidMapFileException.java
+++ riskGameTeam13/src/main/java/com/concordia/riskGame/exception/InvalidMapFileException.java
@@ -2,22 +2,24 @@ package com.concordia.riskGame.exception;
 
 /**
  * This class contains custom exception for Invalid map file.
+ * 
  * @author D_Modi
  */
-public class InvalidMapFileException extends Exception{
-	
-    /**
-     * Default Constructor
-     */
-    public InvalidMapFileException() {}
+public class InvalidMapFileException extends Exception {
 
-    /**
-     * Constructor that accepts a message
-     * @param message : Invalid map file message
-     */
-    public InvalidMapFileException(String message)
-    {
-       super(message);
-    }
+	/**
+	 * Default Constructor
+	 */
+	public InvalidMapFileException() {
+	}
+
+	/**
+	 * Constructor that accepts a message
+	 * 
+	 * @param message : Invalid map file message
+	 */
+	public InvalidMapFileException(String message) {
+		super(message);
+	}
 
 }
