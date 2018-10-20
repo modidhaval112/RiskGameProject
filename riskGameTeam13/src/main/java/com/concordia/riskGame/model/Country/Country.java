a0be7b39eef3029617e9187f55a545ea6747254a
Darwin Anirudh <darwinanirudh@gmail.com>
2018-10-19 21:58:42 -0400

    Added java doc comments.

--- riskGameTeam13/src/main/java/com/concordia/riskGame/model/Country/Country.java
+++ riskGameTeam13/src/main/java/com/concordia/riskGame/model/Country/Country.java
@@ -4,10 +4,10 @@ import java.util.List;
 
 /**
  * Country Class go get and set all properties of a country.
+ * 
  * @author sande
  *
  */
-
 
 public class Country {
 
@@ -17,125 +17,156 @@ public class Country {
 	private int startPixel;
 	private int endPixel;
 	private int armies;
-	
+
 	/***
-	 *Default Constructor 
+	 * Default Constructor
 	 **/
-	public Country() {}
-	
-	
+	public Country() {
+	}
+
 	/***
-	 *This is the parameterized constructor with following parameters
-	 *@param countryName Name of the Country
+	 * This is the parameterized constructor with following parameters
+	 * 
+	 * @param countryName Name of the Country
 	 **/
 	public Country(String countryName) {
-        this.countryName = countryName;
-    }
-	
-	
+		this.countryName = countryName;
+	}
+
 	/***
-	 *This is the parameterized constructor with following parameters
-	 *@param countryName Name of the Country
-	 *@param belongsToContinent Name of the Continent
+	 * This is the parameterized constructor with following parameters
+	 * 
+	 * @param countryName        Name of the Country
+	 * @param belongsToContinent Name of the Continent
 	 **/
-	public Country(String countryName,String belongsToContinent) {
-        this.countryName = countryName;
-        this.belongsToContinent = belongsToContinent;
-    }
-	
+	public Country(String countryName, String belongsToContinent) {
+		this.countryName = countryName;
+		this.belongsToContinent = belongsToContinent;
+	}
+
 	/**
 	 * This is the parameterized constructor with following parameters
-	 * @param countryName Name of the Country
-	 * @param startPixel For displaying the map
-	 * @param endPixel For displaying the map
+	 * 
+	 * @param countryName        Name of the Country
+	 * @param startPixel         For displaying the map
+	 * @param endPixel           For displaying the map
 	 * @param belongsToContinent Name of the Continent
 	 */
-	public Country(String countryName,int startPixel,int endPixel,String belongsToContinent) {
+	public Country(String countryName, int startPixel, int endPixel, String belongsToContinent) {
 		this.countryName = countryName;
 		this.startPixel = startPixel;
 		this.endPixel = endPixel;
 		this.belongsToContinent = belongsToContinent;
 	}
+
 	/**
 	 * method to get the name of the country
-	 * @return countryName :  Name of the country
+	 * 
+	 * @return countryName : Name of the country
 	 */
 	public String getCountryName() {
 		return countryName;
 	}
+
 	/**
 	 * method to set the name of the country
-	 * @param countryName :  Name of the country
+	 * 
+	 * @param countryName : Name of the country
 	 */
 	public void setCountryName(String countryName) {
 		this.countryName = countryName;
 	}
+
 	/**
 	 * method to get the neighbors of this particular country
+	 * 
 	 * @return adjacentCountries : List of neighboring countries
 	 */
 	public List<Country> getNeighbouringCountries() {
 		return neighbouringCountries;
 	}
+
 	/**
 	 * method to set the neighbors of this particular country
+	 * 
 	 * @param adjacentCountries : List of neighboring countries
 	 */
 	public void setNeighbouringCountries(List<Country> adjacentCountries) {
 		this.neighbouringCountries = adjacentCountries;
 	}
+
 	/**
 	 * method to get the continent to which the country belongs
-	 * @return belongsToContinent : Name of the continent to which the country belongs
+	 * 
+	 * @return belongsToContinent : Name of the continent to which the country
+	 *         belongs
 	 */
 	public String getBelongsToContinent() {
 		return belongsToContinent;
 	}
+
 	/**
 	 * method to set the continent to which the country belongs
-	 * @param belongsToContinent : Name of the continent to which the country belongs
+	 * 
+	 * @param belongsToContinent : Name of the continent to which the country
+	 *                           belongs
 	 */
 	public void setBelongsToContinent(String belongsToContinent) {
 		this.belongsToContinent = belongsToContinent;
 	}
+
 	/**
 	 * method to get the startPixel
+	 * 
 	 * @return startPixel : Start Coordinate of the country
 	 */
 	public int getStartPixel() {
 		return startPixel;
 	}
+
 	/**
 	 * method to set the startPixel
+	 * 
 	 * @param startPixel : Start Coordinate of the country
 	 */
 	public void setStartPixel(int startPixel) {
 		this.startPixel = startPixel;
 	}
+
 	/**
 	 * method to get the startPixel
+	 * 
 	 * @return endPixel : End coordinate of the country
 	 */
 	public int getEndPixel() {
 		return endPixel;
 	}
+
 	/**
 	 * method to set the startPixel
+	 * 
 	 * @param endPixel : End coordinate of the country
 	 */
 	public void setEndPixel(int endPixel) {
 		this.endPixel = endPixel;
 	}
 
-
+	/**
+	 * This method returns number of armies.
+	 * 
+	 * @return The number of armies is returned.
+	 */
 	public int getArmies() {
 		return armies;
 	}
 
-
+	/**
+	 * This method set the number of armies.
+	 * 
+	 * @param armies The number of armies.
+	 */
 	public void setArmies(int armies) {
 		this.armies = armies;
 	}
-	
-	
+
 }
