b8e5fdbca724c3bd10db64f13e820f87f9ceee10
Darwin Anirudh <darwinanirudh@gmail.com>
2018-10-19 21:52:31 -0400

    Formatted code.

--- riskGameTeam13/src/main/java/com/concordia/riskGame/entity/GameMap.java
+++ riskGameTeam13/src/main/java/com/concordia/riskGame/entity/GameMap.java
@@ -6,12 +6,12 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import com.concordia.riskGame.model.Country.Country;
 
-
 /**
- * This class contains setters and getters for different attributes of GameMap Object
+ * This class contains setters and getters for different attributes of GameMap
+ * Object
+ * 
  * @author d_modi
  */
 public class GameMap {
@@ -22,14 +22,15 @@ public class GameMap {
 	Map<String, String> continentCountry = new HashMap<>();
 	Set<String> setContinent = new HashSet<>();
 	boolean uniqueCountryCount;
-	int labelCount; 
+	int labelCount;
 	int countryCount;
 	Set<String> setCountry = new HashSet<>();
 	Map<String, List<Country>> mapContinentCountry = new HashMap<>();
 	boolean visitedContinentMapFlag;
-	
+
 	/**
 	 * method to get the neighbors of this particular country
+	 * 
 	 * @return listCountry : List of Country
 	 */
 	public List<Country> getListCountry() {
@@ -38,6 +39,7 @@ public class GameMap {
 
 	/**
 	 * method to set the list of Country
+	 * 
 	 * @param listCountry : list of Country
 	 */
 	public void setListCountry(List<Country> listCountry) {
@@ -46,14 +48,16 @@ public class GameMap {
 
 	/**
 	 * method to get the Country and list of neighboring Countries
+	 * 
 	 * @return mapCountry : map of Country and List of Countries
 	 */
 	public Map<Country, List<Country>> getMapCountry() {
 		return mapCountry;
 	}
-	
+
 	/**
 	 * method to set Country and list of neighboring Countries
+	 * 
 	 * @param mapCountry : map of Country and List of Countries
 	 */
 	public void setMapCountry(Map<Country, List<Country>> mapCountry) {
@@ -62,6 +66,7 @@ public class GameMap {
 
 	/**
 	 * method to get the Label Count for Map, Continent and Territories labels
+	 * 
 	 * @return labelCount : Number of Labels present in the Map file
 	 */
 	public int getLabelCount() {
@@ -70,6 +75,7 @@ public class GameMap {
 
 	/**
 	 * method to set the Label Count for Map, Continent and Territories labels
+	 * 
 	 * @param labelCount : Number of Labels present in the Map file
 	 */
 	public void setLabelCount(int labelCount) {
@@ -77,7 +83,9 @@ public class GameMap {
 	}
 
 	/**
-	 * method to get the Continent and Number of Territories needs to win the continent
+	 * method to get the Continent and Number of Territories needs to win the
+	 * continent
+	 * 
 	 * @return mapWinCount : Continent Name and Win Count for each continent
 	 */
 	public Map<String, Integer> getMapWinCount() {
@@ -85,7 +93,9 @@ public class GameMap {
 	}
 
 	/**
-	 * method to set the Continent and Number of Territories needs to win the continent
+	 * method to set the Continent and Number of Territories needs to win the
+	 * continent
+	 * 
 	 * @param mapWinCount : Continent Name and Win Count for each continent
 	 */
 	public void setMapWinCount(Map<String, Integer> mapWinCount) {
@@ -94,6 +104,7 @@ public class GameMap {
 
 	/**
 	 * method to get the Set of unique Continents
+	 * 
 	 * @return setContinent : Set of Continents
 	 */
 	public Set<String> getSetContinent() {
@@ -102,6 +113,7 @@ public class GameMap {
 
 	/**
 	 * method to set the Set of unique Continents
+	 * 
 	 * @param setContinent : Set of Continents
 	 */
 	public void setSetContinent(Set<String> setContinent) {
@@ -110,6 +122,7 @@ public class GameMap {
 
 	/**
 	 * method to get Country and Continent that belongs to
+	 * 
 	 * @return continentCountry : Country and Continent
 	 */
 	public Map<String, String> getContinentCountry() {
@@ -118,6 +131,7 @@ public class GameMap {
 
 	/**
 	 * method to set Country and Continent that belongs to
+	 * 
 	 * @param continentCountry : Country and Continent
 	 */
 	public void setContinentCountry(Map<String, String> continentCountry) {
@@ -126,14 +140,16 @@ public class GameMap {
 
 	/**
 	 * method to get Unique Country Count to check if it exists in list or not
+	 * 
 	 * @return uniqueCountryCount : true or false according the input list
 	 */
 	public boolean getUniqueCountryCount() {
 		return uniqueCountryCount;
 	}
-	
+
 	/**
 	 * method to set Unique Country Count to check if it exists in list or not
+	 * 
 	 * @param uniqueCountryCount : true or false according the input list
 	 */
 	public void setUniqueCountryCount(boolean uniqueCountryCount) {
@@ -142,6 +158,7 @@ public class GameMap {
 
 	/**
 	 * method to get number of unique country
+	 * 
 	 * @return countryCount : Number of Unique countries
 	 */
 	public int getCountryCount() {
@@ -150,6 +167,7 @@ public class GameMap {
 
 	/**
 	 * method to set number of unique country
+	 * 
 	 * @param countryCount : Number of Unique countries
 	 */
 	public void setCountryCount(int countryCount) {
@@ -158,6 +176,7 @@ public class GameMap {
 
 	/**
 	 * method to get the set of unique countries
+	 * 
 	 * @return setCountry : Set of unique countries
 	 */
 	public Set<String> getSetCountry() {
@@ -166,14 +185,16 @@ public class GameMap {
 
 	/**
 	 * method to set the set of unique countries
+	 * 
 	 * @param setCountry : Set of unique countries
 	 */
 	public void setSetCountry(Set<String> setCountry) {
 		this.setCountry = setCountry;
 	}
-	
+
 	/**
 	 * method to get the Continent and belonging countries
+	 * 
 	 * @return mapContinentCountry : Continent and belonging Countries
 	 */
 	public Map<String, List<Country>> getMapContinentCountry() {
@@ -182,6 +203,7 @@ public class GameMap {
 
 	/**
 	 * method to set the Continent and belonging countries
+	 * 
 	 * @param mapContinentCountry : Continent and belonging Countries
 	 */
 	public void setMapContinentCountry(Map<String, List<Country>> mapContinentCountry2) {
@@ -190,7 +212,9 @@ public class GameMap {
 
 	/**
 	 * method to get if continent is visited or not
-	 * @return visitedContinentMapFlag : True, if continent is visited, otherwise false
+	 * 
+	 * @return visitedContinentMapFlag : True, if continent is visited, otherwise
+	 *         false
 	 */
 	public boolean getVisitedContinentMapFlag() {
 		return visitedContinentMapFlag;
@@ -198,10 +222,12 @@ public class GameMap {
 
 	/**
 	 * method to set if continent is visited or not
-	 * @param visitedContinentMapFlag : True, if continent is visited, otherwise false
+	 * 
+	 * @param visitedContinentMapFlag : True, if continent is visited, otherwise
+	 *                                false
 	 */
 	public void setVisitedContinentMapFlag(boolean visitedContinentMapFlag) {
 		this.visitedContinentMapFlag = visitedContinentMapFlag;
 	}
-	
+
 }
