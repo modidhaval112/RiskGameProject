060657eb7e735d299681d0e778c5d60af38cb106
Darwin Anirudh <darwinanirudh@gmail.com>
2018-10-19 21:55:32 -0400

    Code Alignment.

--- riskGameTeam13/src/main/java/com/concordia/riskGame/model/Continent/Continent.java
+++ riskGameTeam13/src/main/java/com/concordia/riskGame/model/Continent/Continent.java
@@ -6,6 +6,7 @@ import com.concordia.riskGame.model.Country.Country;
 
 /**
  * Continent Class go get and set all properties of a continent.
+ * 
  * @author sande
  *
  */
@@ -14,59 +15,70 @@ public class Continent {
 	private List<Country> countries;
 	private int numberOfCountries;
 	private int continentControlValue;
-	
+
 	/**
-	 * This is a constructor to create the Continent given only the continent. 
+	 * This is a constructor to create the Continent given only the continent.
+	 * 
 	 * @param continentName This is the name of the Continent
 	 */
 	public Continent(String continentName) {
 		this.continentName = continentName;
 	}
-	
-	
+
 	/**
-	 * This is a constructor to assign Continent  Name and its control value
-	 * @param continentName This is the name of the Continent
+	 * This is a constructor to assign Continent Name and its control value
+	 * 
+	 * @param continentName         This is the name of the Continent
 	 * @param continentControlValue This is the control value of the continent
 	 */
-	public Continent(String continentName,int continentControlValue) {
+	public Continent(String continentName, int continentControlValue) {
 		this.continentName = continentName;
 		this.continentControlValue = continentControlValue;
 	}
-	
-	
+
 	/**
-	 * This is a constructor to create the Continent given the continent and countries of that continent.
+	 * This is a constructor to create the Continent given the continent and
+	 * countries of that continent.
+	 * 
 	 * @param continentName This is the name of the Continent
-	 * @param countries This is the list of the countries that belong to that continent.
+	 * @param countries     This is the list of the countries that belong to that
+	 *                      continent.
 	 */
 	public Continent(String continentName, List<Country> countries) {
 		this.continentName = continentName;
-		this.countries=countries;
+		this.countries = countries;
 	}
+
 	/**
 	 * method to get the countries which belong to this continent
+	 * 
 	 * @return String
 	 */
 	public List<Country> getCountries() {
 		return countries;
 	}
+
 	/**
 	 * method to set the countries which belong to this continent
+	 * 
 	 * @param countries
 	 */
 	public void setCountries(List<Country> countries) {
 		this.countries = countries;
 	}
+
 	/**
 	 * method to get the name of the continent
+	 * 
 	 * @return String
 	 */
 	public String getContinentName() {
 		return continentName;
 	}
+
 	/**
 	 * method to set the name of the continent
+	 * 
 	 * @param continentName
 	 */
 	public void setContinentName(String continentName) {
@@ -75,7 +87,8 @@ public class Continent {
 
 	/**
 	 * Get the count of countries in this continent
-	 * @return numberOfCountries 
+	 * 
+	 * @return numberOfCountries
 	 */
 	public int getNumberOfCountries() {
 		return numberOfCountries;
@@ -83,7 +96,8 @@ public class Continent {
 
 	/**
 	 * Set the count of countries in this continent
-	 * @param numberOfCountries 
+	 * 
+	 * @param numberOfCountries
 	 */
 	public void setNumberOfCountries(int numberOfCountries) {
 		this.numberOfCountries = numberOfCountries;
@@ -91,7 +105,8 @@ public class Continent {
 
 	/**
 	 * Get the control value of the Continent
-	 * @return continentControlValue 
+	 * 
+	 * @return continentControlValue
 	 */
 
 	public int getContinentControlValue() {
@@ -100,13 +115,12 @@ public class Continent {
 
 	/**
 	 * Set the control value of the Continent
-	 * @param continentControlValue 
+	 * 
+	 * @param continentControlValue
 	 */
 
 	public void setContinentControlValue(int continentControlValue) {
 		this.continentControlValue = continentControlValue;
 	}
-	
-	
-	
+
 }
