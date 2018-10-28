0124415c393912474de35cb45b92ad7a7abdd1ea
Sai Krishna <saich94@gmail.com>
2018-10-28 14:44:49 -0400

    Added world domination view

--- riskGameTeam13/src/main/java/com/concordia/riskGame/View/WorldDominationView.java
+++ riskGameTeam13/src/main/java/com/concordia/riskGame/View/WorldDominationView.java
@@ -5,27 +5,25 @@ package com.concordia.riskGame.View;
 
 import java.util.Observable;
 import java.util.Observer;
-import com.concordia.riskGame.model.Player.*;
 
+import com.concordia.riskGame.model.Player.Player;
 
 /**
- * This class will display the Game phase details
  * @author saich
  *
  */
-public class PhaseView implements Observer{
+public class WorldDominationView implements Observer {
 
 	/* (non-Javadoc)
 	 * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
 	 */
 	@Override
 	public void update(Observable arg0, Object arg1) {
-       Player player = (Player) arg0;
+	       Player player = (Player) arg0;
+	       System.out.println("In Domination View ");
+			System.out.println("Phase View " + player.phasePrint);
 
-
-		System.out.println("In Phase View ");
-		System.out.println("Phase View " + player.phasePrint);
-
+		
 	}
 
 }
