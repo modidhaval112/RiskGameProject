4d2acd45d9fa204a2de28eb4ef1325460623c45d
modidhaval112 <modidhaval112@gmail.com>
2018-10-31 14:50:41 -0400

    Skeleton code for FortificationTest

--- riskGameTeam13/src/test/java/com/concordia/riskGame/test/FortificationTest.java
+++ riskGameTeam13/src/test/java/com/concordia/riskGame/test/FortificationTest.java
@@ -1,61 +1,72 @@
 package com.concordia.riskGame.test;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
-import org.junit.Test;
 
-import com.concordia.riskGame.control.GameDriver;
+import com.concordia.riskGame.model.Country.Country;
+import com.concordia.riskGame.model.Player.Player;
 
 
 /**
  * This class implements the test cases for testing the computation of
- * reinforcement armies
- * @author Dheeraj As
+ * fortification phase
+ * @author D_Modi
  *
  */
 public class FortificationTest {
-	int noOfCountries1;
-	int noOfCountries2;
-	int noOfCountries3;
-	int result;
+
+	Player p1, p2;
+	Country c1,c2,c3,c4;
+	List<Country> listCountry;
+	
+	/**
+	 * before method for initializing objects
+	 */
 	@Before
-	public void setUp () {
-		noOfCountries1= 10;
-		noOfCountries2= 2;
-		noOfCountries3= 21;
+	public void before () {
+		p1 = new Player();
+		p2 = new Player();
+		c1 = new Country();
+		c2 = new Country();
+		c3 = new Country();
+		c4 = new Country();
+		listCountry = new ArrayList<>();
 		
-		result=0;
+		listCountry.add(c2);
+		listCountry.add(c3);
+		c1.setNeighbouringCountries(listCountry);
+		
+		listCountry.clear();
+		
+		listCountry.add(c3);
+		listCountry.add(c4);
+		c2.setNeighbouringCountries(listCountry);
+		
+		listCountry.clear();
+		
+		listCountry.add(c4);
+		listCountry.add(c1);
+		c3.setNeighbouringCountries(listCountry);
+		
+		listCountry.clear();
+		
+		listCountry.add(c1);
+		listCountry.add(c2);
+		c4.setNeighbouringCountries(listCountry);
+		
+		listCountry.clear();
+		
+		listCountry.add(c1);
+		listCountry.add(c2);
+		p1.setAssignedCountries(listCountry);
+		
+		listCountry.clear();
+		
+		listCountry.add(c3);
+		listCountry.add(c4);
+		p2.setAssignedCountries(listCountry);
 	}
-	
-	/**
-	 * This method test if the calculation of armies is correct when number of 
-	 * countries is greater than 3
-	 */
-	@Test
-	public void reinforcementArmyCalculationTest1() {
-		
-		GameDriver gameDriverObject= new GameDriver();
-		
-		result= gameDriverObject.calculateReiforcementArmies(noOfCountries1);
-		assertEquals(3,result);
-		
-		result= gameDriverObject.calculateReiforcementArmies(noOfCountries3);
-		assertEquals(7,result);
-		
-	}
-	
-	/**
-	 * This method tests if the This method test if the calculation of armies is correct when number of 
-	 * countries is less than 3
-	 */
-	@Test
-	public void reinforcementArmyCalculationTest2() {
-		GameDriver gameDriverObject= new GameDriver();
-		
-		result= gameDriverObject.calculateReiforcementArmies(noOfCountries2);
-		assertEquals(3,result);
-	}
-	
 	
 }
