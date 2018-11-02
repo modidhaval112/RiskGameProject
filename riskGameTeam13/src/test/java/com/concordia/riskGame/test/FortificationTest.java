8d9a70ae78b23a676145ace48fb5d57fed2b76f6
modidhaval112 <modidhaval112@gmail.com>
2018-11-01 20:50:00 -0400

    initial commit

---
+++ riskGameTeam13/src/test/java/com/concordia/riskGame/test/FortificationTest.java
@@ -0,0 +1,11 @@
+package com.concordia.riskGame.test;
+
+
+
+/**
+ * This class implements the test cases for testing the computation of
+ * fortification phase
+ * @author D_Modi
+ *
+ */
+public class FortificationTest {}
