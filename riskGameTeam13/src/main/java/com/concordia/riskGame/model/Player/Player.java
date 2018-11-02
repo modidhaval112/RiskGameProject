eaaf0f7a36d73e8d631247e7da2d6e97efb08416
modidhaval112 <modidhaval112@gmail.com>
2018-11-02 15:21:09 -0400

    Used setErrorMessage to set the message

--- riskGameTeam13/src/main/java/com/concordia/riskGame/model/Player/Player.java
+++ riskGameTeam13/src/main/java/com/concordia/riskGame/model/Player/Player.java
@@ -458,6 +458,7 @@ public class Player extends Observable implements Serializable {
 
 				player.setAssignedCountries(assignedCountriesClone);
 				System.out.println("##### Armies have been moved between countries ###### ");
+				setErrorMesage("Armies have been moved between countries");
 				return player;
 			} catch (Exception e) {
 				System.out.println("Exception Message " + e.getMessage());
