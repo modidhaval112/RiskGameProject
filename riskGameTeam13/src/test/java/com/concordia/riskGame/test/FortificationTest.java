e79542052dbb484bf6fa6064765cd525d977d7d7
modidhaval112 <modidhaval112@gmail.com>
2018-11-02 06:23:21 -0400

    before method has been added

--- riskGameTeam13/src/test/java/com/concordia/riskGame/test/FortificationTest.java
+++ riskGameTeam13/src/test/java/com/concordia/riskGame/test/FortificationTest.java
@@ -1,5 +1,18 @@
 package com.concordia.riskGame.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
+
+import com.concordia.riskGame.model.Country.Country;
+import com.concordia.riskGame.model.Map.MapContents;
+import com.concordia.riskGame.model.Player.Player;
 
 
 /**
@@ -8,4 +21,76 @@ package com.concordia.riskGame.test;
  * @author D_Modi
  *
  */
-public class FortificationTest {}
+public class FortificationTest {
+
+	Player p1, p2;
+	Country c1,c2,c3,c4;
+	List<Country> listCountry, listCountry1;
+	MapContents mapContents;
+	
+	private String initiateValue;
+    private String country1;
+    private String country2;
+    private String endValue;
+    
+    @Rule
+    public final TextFromStandardInputStream systemInMock
+      = TextFromStandardInputStream.emptyStandardInputStream();
+	
+	/**
+	 * before method for initializing objects
+	 */
+	@Before
+	public void before () {
+		p1 = new Player("p1");
+		p2 = new Player("p2");
+		c1 = new Country("c1");
+		c2 = new Country("c2");
+		c3 = new Country("c3");
+		c4 = new Country("c4");
+		listCountry = new ArrayList<>();
+		listCountry1 = new ArrayList<>();
+		mapContents = MapContents.getInstance();
+		
+		p1.setTotalArmies(40);
+		p2.setTotalArmies(40);
+		c1.setArmies(10);
+		c2.setArmies(10);
+		c3.setArmies(10);
+		c4.setArmies(10);
+		
+		listCountry.add(c2);
+		listCountry.add(c3);
+		c1.setNeighbouringCountries(listCountry);
+		
+		listCountry = new ArrayList<>();
+		
+		listCountry.add(c3);
+		listCountry.add(c4);
+		c2.setNeighbouringCountries(listCountry);
+		
+		listCountry = new ArrayList<>();
+		
+		listCountry.add(c4);
+		listCountry.add(c1);
+		c3.setNeighbouringCountries(listCountry);
+		
+		listCountry = new ArrayList<>();
+		
+		listCountry.add(c1);
+		listCountry.add(c2);
+		c4.setNeighbouringCountries(listCountry);
+		
+		listCountry = new ArrayList<>();
+		
+		listCountry.add(c1);
+		listCountry.add(c2);
+		p1.setAssignedCountries(listCountry);
+						
+		listCountry1.add(c3);
+		listCountry1.add(c4);
+		p2.setAssignedCountries(listCountry1);
+		
+	}
+	
+}
