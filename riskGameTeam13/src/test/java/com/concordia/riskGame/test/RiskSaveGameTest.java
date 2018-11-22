ab1109e74cae448a8173f5e1e0efc03f342d950b
modidhaval112 <modidhaval112@gmail.com>
2018-11-22 18:16:11 -0500

    Test case for checking if data is added correctly or not, has been added

--- riskGameTeam13/src/test/java/com/concordia/riskGame/test/RiskSaveGameTest.java
+++ riskGameTeam13/src/test/java/com/concordia/riskGame/test/RiskSaveGameTest.java
@@ -1,11 +1,16 @@
 package com.concordia.riskGame.test;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -13,7 +18,7 @@ import org.junit.Test;
 import org.junit.contrib.java.lang.system.TextFromStandardInputStream;
 import static org.junit.Assert.assertTrue;
 
-
+import com.concordia.riskGame.model.Continent.Continent;
 import com.concordia.riskGame.model.Country.Country;
 import com.concordia.riskGame.model.Map.MapContents;
 import com.concordia.riskGame.model.Player.Player;
@@ -21,10 +26,16 @@ import com.concordia.riskGame.model.save.RiskSaveGame;
 
 public class RiskSaveGameTest {
 	
-	Player p1, p2;
-	Country c1,c2,c3,c4;
-	List<Country> listCountry, listCountry1;
-	MapContents mapContents;
+	private Player p1, p2;
+	private Country c1,c2,c3,c4;
+	private Continent con1, con2;
+	private List<Player> listPlayer;
+	private List<Country> listCountry, listCountry1;
+	private Map<Continent, List<Country>> mapContinentCountry;
+	private Map<Country, List<Country>> mapCountryAndNBCountry;
+	private MapContents mapContents;
+	private RiskSaveGame riskSaveGame;
+	private String expectedFileName;
 	
     @Rule
     public final TextFromStandardInputStream systemInMock
@@ -41,8 +52,13 @@ public class RiskSaveGameTest {
 		c2 = new Country("c2");
 		c3 = new Country("c3");
 		c4 = new Country("c4");
+		con1 = new Continent("con1");
+		con2 = new Continent("con2");
 		listCountry = new ArrayList<>();
 		listCountry1 = new ArrayList<>();
+		listPlayer = new ArrayList<>();
+		mapContinentCountry = new HashMap<>();
+		mapCountryAndNBCountry = new HashMap<>();
 		mapContents = MapContents.getInstance();
 		
 		p1.setTotalArmies(40);
@@ -79,6 +95,9 @@ public class RiskSaveGameTest {
 		mapContents.getCountryAndNeighbors().put(c4, listCountry);
 		
 		listCountry = new ArrayList<>();
+
+		mapContents.getContinentAndItsCountries().put(con1, new ArrayList<Country>(Arrays.asList(c1,c2)));
+		mapContents.getContinentAndItsCountries().put(con2, new ArrayList<Country>(Arrays.asList(c3,c4)));
 		
 		listCountry.add(c1);
 		listCountry.add(c2);
@@ -88,6 +107,15 @@ public class RiskSaveGameTest {
 		listCountry1.add(c4);
 		p2.setAssignedCountries(listCountry1);
 		
+		listPlayer.add(p1);
+		listPlayer.add(p2);
+		mapContents.setPlayerList(listPlayer);
+		
+		riskSaveGame = new RiskSaveGame();
+		riskSaveGame.saveGame(mapContents);
+		
+
+		
 	}
 	
 	/**
@@ -95,13 +123,11 @@ public class RiskSaveGameTest {
 	 */
 	@Test
 	public void testFileCreation() {
-		RiskSaveGame riskSaveGame = new RiskSaveGame();
-		riskSaveGame.saveGame(mapContents);
-		
+
 		boolean flag = false;
 		DateFormat sdf = new SimpleDateFormat("yyyyMMddHH:mm:ss");
 		Date date = new Date();
-		String expectedFileName = sdf.format(date);
+		expectedFileName = sdf.format(date);
 		expectedFileName = expectedFileName.replaceAll(":", "");
 		
 		File folder = new File("C:\\SaveGame\\");
@@ -117,6 +143,36 @@ public class RiskSaveGameTest {
 			}
 		}
 	    assertTrue(flag);
+	}
+	
+	/**
+	 * Test method to check if saved game file has been created or not 
+	 */
+	@Test
+	public void testCheckFileData() {
+
+		try {
+			boolean flag = false;
+			FileInputStream savedFile = new FileInputStream("C:\\SaveGame\\" + expectedFileName);
+			ObjectInputStream restore = new ObjectInputStream(savedFile);
+			Object obj = restore.readObject();
+			System.out.println("####### Saved file object is ####### : " + obj.toString());
+
+			MapContents mapContentObject = (MapContents) obj;
+			List<Player> listplayer1 = mapContentObject.getPlayerList();
+			List<Country> listContinentCountry1 = mapContentObject.getContinentAndItsCountries().get(con1);
+			List<Country> listCountryNBCountry1 = mapContentObject.getCountryAndNeighbors().get(c1);
+			
+			if(listplayer1.size() == 2 && listContinentCountry1.size() == 2 && listCountryNBCountry1.size() == 2) {
+				flag = true;
+			}
+			
+			assertTrue(flag);
+			
+		} catch (Exception e) {
+			System.out.println("File Not Found");
+		}
+
 	}
 
 }
