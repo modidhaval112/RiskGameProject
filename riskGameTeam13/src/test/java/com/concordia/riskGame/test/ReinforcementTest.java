0c86d21db42d5a490fc84fcbc15d081d039126a9
Darwin Anirudh <darwinanirudh@gmail.com>
2018-11-30 09:46:55 -0500

    Added Test Case for refractoring made for print all countries of the player.

--- riskGameTeam13/src/test/java/com/concordia/riskGame/test/ReinforcementTest.java
+++ riskGameTeam13/src/test/java/com/concordia/riskGame/test/ReinforcementTest.java
@@ -2,10 +2,15 @@ package com.concordia.riskGame.test;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
 import com.concordia.riskGame.control.GameDriver;
+import com.concordia.riskGame.model.Country.Country;
+import com.concordia.riskGame.model.Player.Player;
 
 
 /**
@@ -19,6 +24,9 @@ public class ReinforcementTest {
 	int noOfCountries2;
 	int noOfCountries3;
 	int result;
+	Player playerTest;
+	Country c1;
+	List<Country> countryList;
 	/**
 	 * Initializing the Context
 	 */
@@ -28,6 +36,12 @@ public class ReinforcementTest {
 		noOfCountries2= 2;
 		noOfCountries3= 21;
 		result=0;
+		playerTest = new Player();
+		countryList = new ArrayList();
+		c1 = new Country("India");
+		countryList.add(c1);
+		playerTest.setAssignedCountries(countryList);
+		
 	}
 	
 	/**
@@ -57,6 +71,23 @@ public class ReinforcementTest {
 		result= gameDriverObject.calculateReiforcementArmies(noOfCountries2);
 		assertEquals(3,result);
 	}
+	
+	/**
+	 * 
+	 */
+	@Test
+	public void printCountriesOwnedByPlayer() {
+	
+	String name = playerTest.getAssignedCountries().get(0).getCountryName();
+	int countryCount = playerTest.getAssignedCountries().size();
+	
+	assertEquals("India",name);
+	assertEquals(1,countryCount);
+	
+	
+	
+	}
+	
 	
 	
 }
